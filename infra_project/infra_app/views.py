f34766742a66545c4b2040de559c3a5077b15194
Никита Татауров <sugatosansiro84@gmail.com>
2023-02-21 22:11:38 +0300

    8th commit for Workflow settings

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Ура! У меня получилось!')
+    return HttpResponse('Ура! У меня снова получилось!')
 
 
 def second_page(request):
